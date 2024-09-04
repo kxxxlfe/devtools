09798e817d4bce2d9426ef8188cf63c6f8ee0cfc
yu.hu <yu.hu@happyelements.com>
2024-09-04 17:04:40 +0800

    聚焦重连

--- packages/shell-chrome/src/devtools.js
+++ packages/shell-chrome/src/devtools.js
@@ -3,38 +3,54 @@
 import { initDevTools } from '@front'
 import Bridge from '@utils/bridge'
 
-initDevTools({
+let port
+let disconnected
+const createPort = function() {
+  disconnected = false
+  port = chrome.runtime.connect({
+    name: '' + chrome.devtools.inspectedWindow.tabId,
+  })
+  port.onDisconnect.addListener((...args) => {
+    console.log('disconnected service-worker', args, chrome.runtime.lastError)
+    disconnected = true
+  })
+}
+window.createPort = createPort
+chrome.tabs.onActivated.addListener(function({ tabId }) {
+  if (tabId !== chrome.devtools.inspectedWindow.tabId) {
+    return
+  }
+  // 聚焦重连
+  if (port && disconnected) {
+    createPort()
+  }
+})
 
+initDevTools({
   /**
    * Inject backend, connect to background, and send back the bridge.
    *
    * @param {Function} cb
    */
 
-  connect (cb) {
+  connect(cb) {
     // 1. inject backend code into page
     injectScript(chrome.runtime.getURL('build/backend.js'), () => {
       // 2. connect to background to setup proxy
-      const port = chrome.runtime.connect({
-        name: '' + chrome.devtools.inspectedWindow.tabId
-      })
-      let disconnected = false
-      port.onDisconnect.addListener(() => {
-        disconnected = true
-      })
+      createPort()
 
       const bridge = new Bridge({
-        listen (fn) {
+        listen(fn) {
           port.onMessage.addListener(fn)
         },
-        send (data) {
+        send(data) {
           if (!disconnected) {
             // if (process.env.NODE_ENV !== 'production') {
             //   console.log('[chrome] devtools -> backend', data)
             // }
             port.postMessage(data)
           }
-        }
+        },
       })
       // 3. send a proxy API to the panel
       cb(bridge)
@@ -47,9 +63,9 @@ initDevTools({
    * @param {Function} reloadFn
    */
 
-  onReload (reloadFn) {
+  onReload(reloadFn) {
     chrome.devtools.network.onNavigated.addListener(reloadFn)
-  }
+  },
 })
 
 /**
@@ -60,7 +76,7 @@ initDevTools({
  * @param {Function} cb
  */
 
-function injectScript (scriptName, cb) {
+function injectScript(scriptName, cb) {
   const src = `
     (function() {
       var script = document.constructor.prototype.createElement.call(document, 'script');
@@ -69,7 +85,7 @@ function injectScript (scriptName, cb) {
       script.parentNode.removeChild(script);
     })()
   `
-  chrome.devtools.inspectedWindow.eval(src, function (res, err) {
+  chrome.devtools.inspectedWindow.eval(src, function(res, err) {
     if (err) {
       console.log(err)
     }
