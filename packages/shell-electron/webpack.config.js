65e5f5c225d1c9f304c9835fc0d4f772bf1db109
yu.hu <yu.hu@happyelements.com>
2024-11-05 10:03:23 +0800

    webpackv5

--- packages/shell-electron/webpack.config.js
+++ packages/shell-electron/webpack.config.js
@@ -5,19 +5,22 @@ const target = {
   chrome: 52,
   firefox: 48,
   safari: 9,
-  ie: 11
+  ie: 11,
 }
 
-module.exports = createConfig({
-  entry: {
-    devtools: './src/devtools.js',
-    backend: './src/backend.js',
-    hook: './src/hook.js'
+module.exports = createConfig(
+  {
+    entry: {
+      devtools: './src/devtools.js',
+      backend: './src/backend.js',
+      hook: './src/hook.js',
+    },
+    output: {
+      path: path.join(__dirname, '/build'),
+      publicPath: '/build/',
+      filename: '[name].js',
+    },
+    devtool: 'cheap-module-source-map',
   },
-  output: {
-    path: path.join(__dirname, '/build'),
-    publicPath: '/build/',
-    filename: '[name].js'
-  },
-  devtool: '#cheap-module-source-map'
-}, target)
+  target
+)
