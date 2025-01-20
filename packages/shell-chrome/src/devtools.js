79fd67401190349d08ca57edca9701c458cbc237
yu.hu <yu.hu@happyelements.com>
2025-01-20 10:50:56 +0800

    injectScript没有生效,尝试解决

--- packages/shell-chrome/src/devtools.js
+++ packages/shell-chrome/src/devtools.js
@@ -2,6 +2,7 @@
 
 import { initDevTools } from '@front'
 import Bridge from '@utils/bridge'
+import { waitTime } from '@utils/tools'
 import '@utils/ext-bridge/devtool'
 
 initDevTools({
@@ -11,34 +12,34 @@ initDevTools({
    * @param {Function} cb
    */
 
-  connect(cb) {
+  async connect(cb) {
     // 1. inject backend code into page
-    injectScript(chrome.runtime.getURL('build/backend.js'), () => {
-      // 2. connect to background to setup proxy
-      const port = chrome.runtime.connect({
-        name: '' + chrome.devtools.inspectedWindow.tabId,
-      })
-      let disconnected = false
-      port.onDisconnect.addListener(() => {
-        disconnected = true
-      })
+    await injectScript(chrome.runtime.getURL('build/backend.js'))
 
-      const bridge = new Bridge({
-        listen(fn) {
-          port.onMessage.addListener(fn)
-        },
-        send(data) {
-          if (!disconnected) {
-            // if (process.env.NODE_ENV !== 'production') {
-            //   console.log('[chrome] devtools -> backend', data)
-            // }
-            port.postMessage(data)
-          }
-        },
-      })
-      // 3. send a proxy API to the panel
-      cb(bridge)
+    // 2. connect to background to setup proxy
+    const port = chrome.runtime.connect({
+      name: '' + chrome.devtools.inspectedWindow.tabId,
     })
+    let disconnected = false
+    port.onDisconnect.addListener(() => {
+      disconnected = true
+    })
+
+    const bridge = new Bridge({
+      listen(fn) {
+        port.onMessage.addListener(fn)
+      },
+      send(data) {
+        if (!disconnected) {
+          // if (process.env.NODE_ENV !== 'production') {
+          //   console.log('[chrome] devtools -> backend', data)
+          // }
+          port.postMessage(data)
+        }
+      },
+    })
+    // 3. send a proxy API to the panel
+    cb(bridge)
   },
 
   /**
@@ -60,7 +61,8 @@ initDevTools({
  * @param {Function} cb
  */
 
-function injectScript(scriptName, cb) {
+async function injectScript(scriptName) {
+  await waitTime(0) // wait page loaded
   const src = `
     (function() {
       var script = document.constructor.prototype.createElement.call(document, 'script');
@@ -69,10 +71,13 @@ function injectScript(scriptName, cb) {
       script.parentNode.removeChild(script);
     })()
   `
-  chrome.devtools.inspectedWindow.eval(src, function (res, err) {
-    if (err) {
-      console.log(err)
-    }
-    cb()
+
+  return new Promise(resolve => {
+    chrome.devtools.inspectedWindow.eval(src, function (res, err) {
+      if (err) {
+        console.log(err)
+      }
+      resolve()
+    })
   })
 }
