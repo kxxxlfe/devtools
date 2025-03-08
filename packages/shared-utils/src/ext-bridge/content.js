2afbce2d8cfb0f90ffea17120f80c1dca9a74535
yu.hu <yu.hu@happyelements.com>
2025-03-08 10:19:24 +0800

    拆分bridge包

--- packages/shared-utils/src/ext-bridge/content.js
+++ packages/shared-utils/src/ext-bridge/content.js
@@ -1,44 +1,6 @@
 /* 
   web环境：content <=> web
 */
-import { MsgDef, isBridgeMessage, WinPost, Plat } from './const'
+import { ContentBridge } from '@yuhufe/browser-bridge'
 
-const win = new WinPost({ plat: Plat.content })
-
-// web请求转发devtool
-window.addEventListener('message', async function (evt) {
-  const msgdata = win.parseMsg(evt.data)
-  if (!isBridgeMessage(msgdata)) {
-    return
-  }
-  if (msgdata.type !== MsgDef.request) {
-    return
-  }
-  const handle = chrome.runtime.sendMessage(msgdata)
-
-  if (msgdata.needResponse) {
-    const res = await handle
-    win.response({ request: evt.data, response: res })
-  }
-})
-
-// devtool请求转发web
-chrome.runtime.onMessage.addListener((msgdata, source, sendResponse) => {
-  if (!isBridgeMessage(msgdata)) {
-    return
-  }
-  if (msgdata.type !== MsgDef.request) {
-    return
-  }
-
-  const handle = win.post(msgdata)
-
-  if (msgdata.needResponse) {
-    ;(async function () {
-      const res = await handle
-      sendResponse(res)
-    })()
-
-    return true
-  }
-})
+export const contentBridge = new ContentBridge()
--- packages/shared-utils/src/ext-bridge/web.js
+++ packages/shared-utils/src/ext-bridge/web.js
@@ -1,62 +1,17 @@
 /* 
   web环境：content <=> web
 */
-import { MsgDef, Plat, EventHandle, makeRequest, makeResponse, WinPost, isBridgeMessage } from './const'
-
-const win = new WinPost({ plat: Plat.web })
-
-class WebBridge extends EventHandle {
-  callbacks = {}
-  plat = Plat.web
-  Plat = Plat
-
-  constructor() {
-    super()
-    this.onRequest = this.onRequest.bind(this)
-    window.addEventListener('message', this.onRequest)
-  }
-
-  // 无response
-  send(path, params) {
-    const msg = makeRequest({ plat: this.plat, path, params })
-
-    return win.post(msg)
-  }
-  request(path, params) {
-    const msg = makeRequest({ plat: this.plat, path, params })
-    msg.needResponse = true
-    return win.post(msg)
-  }
-  // 处理请求，负责返回
-  async onRequest(evt) {
-    const msgdata = win.parseMsg(evt.data)
-    if (!isBridgeMessage(msgdata)) {
-      return
-    }
-    if (!msgdata.target?.startsWith(this.plat)) {
-      return
-    }
-    const uuid = msgdata.uuid
-
-    if (msgdata.type !== MsgDef.request) {
-      return
-    }
-
-    const request = msgdata
-    const { path, params } = request
-    const res = await this.trigger(path, params)
-    win.response({ request: evt.data, response: makeResponse({ plat: this.plat, data: res, request }) })
-  }
-}
+import { WebBridge, Plat } from '@yuhufe/browser-bridge'
 
 export const bridge = new WebBridge()
+bridge.Plat = Plat
 
-window.webBridge = bridge
-window.testBridge = async function () {
-  const res = await bridge.request(`${Plat.devtool}/test`, { from: 'web' })
-  console.log('response', res)
-}
-bridge.on(`${Plat.web}/test`, function (info) {
-  console.log(info)
-  return { result: 'web ok' }
-})
+// window.webBridge = bridge
+// window.testBridge = async function () {
+//   const res = await bridge.request(`${Plat.devtool}/test`, { from: 'web' })
+//   console.log('response', res)
+// }
+// bridge.on(`${Plat.web}/test`, function (info) {
+//   console.log(info)
+//   return { result: 'web ok' }
+// })
