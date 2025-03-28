650fa4098436804a371e86cc03c4f14e1b4593e5
yu.hu <yu.hu@happyelements.com>
2025-03-28 10:53:21 +0800

    不暴露错误

--- packages/shared-utils/src/shared-data.js
+++ packages/shared-utils/src/shared-data.js
@@ -69,7 +69,7 @@ export function init(params) {
                 clearInterval(initRetryInterval)
               })
               .catch(e => {
-                debug('shared-data:ready', e.message)
+                debug('shared-data:ready catch', e.message)
               })
           initRetryCount = 0
           clearInterval(initRetryInterval)
