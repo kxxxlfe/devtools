f85fe0706289d1c64f1ca1d0a6c01932e7780c93
yu.hu <yu.hu@happyelements.com>
2025-05-04 11:12:29 +0800

    判断属性是不是响应式的

--- packages/app-backend/src/process.js
+++ packages/app-backend/src/process.js
@@ -3,6 +3,21 @@ import { isRef, isReadonly } from 'vue'
 import { camelize, getComponentName, getCustomRefDetails } from '@utils/util'
 import SharedData from '@utils/shared-data'
 
+// 判断数据是否响应式
+const checkReact = function ({ key, val, host }) {
+  if (typeof val === 'object') {
+    return isRef(val) || isReactive(val)
+  }
+
+  const descriptor = Object.getOwnPropertyDescriptor(host, key)
+
+  if (descriptor?.get && descriptor?.set) {
+    return true
+  }
+
+  return
+}
+
 export function getInstanceState(instance) {
   return processProps(instance).concat(
     processState(instance),
