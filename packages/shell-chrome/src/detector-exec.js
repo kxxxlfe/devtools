94449a122fcd2390321c5d1cfcb0805a32e2ee80
yu.hu <yu.hu@happyelements.com>
2025-06-05 10:12:48 +0800

    检测到Vue分发出去

--- packages/shell-chrome/src/detector-exec.js
+++ packages/shell-chrome/src/detector-exec.js
@@ -28,6 +28,9 @@ function detect(win) {
     // Method 2: Scan all elements inside document
     const Vue = detectVue()
     if (Vue) {
+      if (globalThis.__VUE_DEVTOOLS_GLOBAL_HOOK__ && !globalThis.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue) {
+        globalThis.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = Vue
+      }
       win.postMessage(
         {
           devtoolsEnabled: Vue.config.devtools,
