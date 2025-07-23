f6496d41f89b6ea96fb33b1a4caa273b33fe4e82
yu.hu <yu.hu@happyelements.com>
2025-07-23 14:01:53 +0800

    需要使用use

--- packages/app-frontend/src/views/settings/GlobalPreferences.vue
+++ packages/app-frontend/src/views/settings/GlobalPreferences.vue
@@ -1,97 +1,49 @@
 <template>
   <div class="global-preferences preferences">
     <VueFormField title="Normalize component names">
-      <VueGroup
-        v-model="$shared.componentNameStyle"
-        class="extend"
-      >
-        <VueGroupButton
-          value="original"
-          label="Original name"
-        />
-        <VueGroupButton
-          value="class"
-          label="Pascal case"
-        />
-        <VueGroupButton
-          value="kebab"
-          label="Kebab case"
-        />
+      <VueGroup v-model="sharedData.componentNameStyle" class="extend">
+        <VueGroupButton value="original" label="Original name" />
+        <VueGroupButton value="class" label="Pascal case" />
+        <VueGroupButton value="kebab" label="Kebab case" />
       </VueGroup>
     </VueFormField>
 
     <VueFormField title="Theme">
-      <VueGroup
-        v-model="$shared.theme"
-        class="extend"
-      >
-        <VueGroupButton
-          value="auto"
-          label="Auto"
-        />
-        <VueGroupButton
-          value="light"
-          label="Light"
-        />
-        <VueGroupButton
-          value="dark"
-          label="Dark"
-        />
-        <VueGroupButton
-          value="high-contrast"
-          label="High contrast"
-        />
+      <VueGroup v-model="sharedData.theme" class="extend">
+        <VueGroupButton value="auto" label="Auto" />
+        <VueGroupButton value="light" label="Light" />
+        <VueGroupButton value="dark" label="Dark" />
+        <VueGroupButton value="high-contrast" label="High contrast" />
       </VueGroup>
     </VueFormField>
 
     <VueFormField title="Display density">
-      <VueGroup
-        v-model="$shared.displayDensity"
-        class="extend"
-      >
-        <VueGroupButton
-          value="auto"
-          label="Auto"
-        />
-        <VueGroupButton
-          value="low"
-          label="Low"
-        />
-        <VueGroupButton
-          value="high"
-          label="High"
-        />
+      <VueGroup v-model="sharedData.displayDensity" class="extend">
+        <VueGroupButton value="auto" label="Auto" />
+        <VueGroupButton value="low" label="Low" />
+        <VueGroupButton value="high" label="High" />
       </VueGroup>
     </VueFormField>
 
-    <VueFormField
-      title="Editable props"
-    >
-      <VueSwitch v-model="$shared.editableProps">
-        Enable
-      </VueSwitch>
+    <VueFormField title="Editable props">
+      <VueSwitch v-model="sharedData.editableProps">Enable</VueSwitch>
       <template #subtitle>
-        <VueIcon
-          icon="warning"
-          class="medium"
-        />
+        <VueIcon icon="warning" class="medium" />
         May print warnings in the console
       </template>
     </VueFormField>
 
     <VueFormField title="Time Format">
       <VueSwitch
-        :value="$shared.timeFormat === 'ms'"
-        @update="value => $shared.timeFormat = value ? 'ms' : 'default'"
+        :value="sharedData.timeFormat === 'ms'"
+        @update="value => (sharedData.timeFormat = value ? 'ms' : 'default')"
       >
         Display milliseconds
       </VueSwitch>
     </VueFormField>
 
     <VueFormField title="Detected Vue message">
-      <VueSwitch v-model="$shared.logDetected">
-        Display in browser console
-      </VueSwitch>
+      <VueSwitch v-model="sharedData.logDetected">Display in browser console</VueSwitch>
     </VueFormField>
 
     <VueFormField>
@@ -99,25 +51,14 @@
         New Vuex backend
         <NewTag :version="2" />
       </template>
-      <VueSwitch v-model="$shared.vuexNewBackend">
-        Enable
-      </VueSwitch>
-      <template #subtitle>
-        Faster and less memory-intensive
-      </template>
+      <VueSwitch v-model="sharedData.vuexNewBackend">Enable</VueSwitch>
+      <template #subtitle>Faster and less memory-intensive</template>
     </VueFormField>
 
-    <VueFormField
-      title="Autoload Vuex state"
-    >
-      <VueSwitch v-model="$shared.vuexAutoload">
-        Enable
-      </VueSwitch>
+    <VueFormField title="Autoload Vuex state">
+      <VueSwitch v-model="sharedData.vuexAutoload">Enable</VueSwitch>
       <template #subtitle>
-        <VueIcon
-          icon="warning"
-          class="medium"
-        />
+        <VueIcon icon="warning" class="medium" />
         May impact performance or cause crashes
       </template>
     </VueFormField>
@@ -127,19 +68,23 @@
         Group getters by module
         <NewTag :version="2" />
       </template>
-      <VueSwitch v-model="$shared.vuexGroupGettersByModule">
-        Enable
-      </VueSwitch>
+      <VueSwitch v-model="sharedData.vuexGroupGettersByModule">Enable</VueSwitch>
     </VueFormField>
   </div>
 </template>
 
 <script>
 import NewTag from './NewTag.vue'
+import { useSharedData } from '@utils/shared-data'
 
 export default {
   components: {
-    NewTag
-  }
+    NewTag,
+  },
+  setup(props, { emit }) {
+    const { sharedData } = useSharedData()
+
+    return { sharedData }
+  },
 }
 </script>
