2edd68d51eaf5dfd272ba93867e4eb77475ae998
yu.hu <yu.hu@happyelements.com>
2025-08-16 11:34:17 +0800

    提升content-script执行时间

--- packages/shell-chrome/src/hook.js
+++ packages/shell-chrome/src/hook.js
@@ -1,6 +1,12 @@
+// devtools对象占位
+const immScript = document.createElement('script')
+immScript.textContent = `window.__VUE_DEVTOOLS_GLOBAL_HOOK__ = {};`
+;(document.head || document.documentElement).prepend(immScript)
+
+// 初始化hook
 const script = document.createElement('script')
 script.src = chrome.runtime.getURL('build/hook-exec.js')
 script.onload = () => {
   script.remove()
 }
-;(document.head || document.documentElement).appendChild(script)
+;(document.head || document.documentElement).prepend(script)
