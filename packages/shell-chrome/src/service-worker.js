f90210f80f044e653cb8ec3c31e4863390f3720d
yu.hu <yu.hu@happyelements.com>
2025-08-16 12:56:15 +0800

    早占位

--- packages/shell-chrome/src/service-worker.js
+++ packages/shell-chrome/src/service-worker.js
@@ -130,3 +130,19 @@ if (chrome.contextMenus) {
     })
   })
 }
+
+// 监听页面开始加载
+chrome.webNavigation.onCommitted.addListener(details => {
+  // 只在主框架 (frameId === 0) 注入
+  if (details.frameId === 0) {
+    chrome.scripting.executeScript({
+      target: { tabId: details.tabId },
+      world: 'MAIN', // 在页面主世界运行，能直接改 window
+      injectImmediately: true, // 等价于 document_start
+      func: () => {
+        // 抢在vue前注入
+        globalThis.__VUE_DEVTOOLS_GLOBAL_HOOK__ = globalThis.__VUE_DEVTOOLS_GLOBAL_HOOK__ || { emit() {}, on() {} }
+      },
+    })
+  }
+})
