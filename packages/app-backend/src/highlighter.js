bce9b4e9e6b17f9213308a46fc95c5d667e18b0d
yu.hu <yu.hu@happyelements.com>
2025-08-16 11:54:21 +0800

    路径错误

--- packages/app-backend/src/highlighter.js
+++ packages/app-backend/src/highlighter.js
@@ -1,6 +1,6 @@
 import { inDoc, getComponentName, getComponentDisplayName } from '@utils/util'
 import SharedData from '@utils/shared-data'
-import { checkVisibility } from '@/utils/tools'
+import { checkVisibility } from '@utils/tools'
 import { isBrowser, target } from '@utils/env'
 import { getInstanceName } from './process'
 
