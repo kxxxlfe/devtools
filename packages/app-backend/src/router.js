442ccb4ad09988f9bc6a6d67a811d62844f3cbe2
yu.hu <yu.hu@happyelements.com>
2026-02-19 21:02:19 +0800

    移除Vue依赖

--- packages/app-backend/src/router.js
+++ packages/app-backend/src/router.js
@@ -2,7 +2,7 @@ import { stringify } from '@utils/util'
 import { bridge as exBridge, api } from './bridge'
 import sharedData from '@utils/shared-data'
 
-export function initRouterBackend(Vue, rootInstances) {
+export function initRouterBackend(rootInstances) {
   const getSnapshot = () => {
     const routeChanges = []
     rootInstances.forEach(instance => {
