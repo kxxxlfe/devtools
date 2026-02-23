1ccb9c140bc3566353f72b826195208bcdd42c4a
yu.hu <yu.hu@happyelements.com>
2026-02-23 11:08:02 +0800

    迁移相关带阿米

---
+++ packages/app-backend/src/utils/flush.ts
@@ -0,0 +1,24 @@
+// 存放一些过程变量
+export function getRenderKey(value) {
+  if (value == null) return
+  const type = typeof value
+  if (type === 'number') {
+    return value
+  } else if (type === 'string') {
+    return `'${value}'`
+  } else if (Array.isArray(value)) {
+    return 'Array'
+  } else {
+    return 'Object'
+  }
+}
+
+// 多次flat
+export function flatten(items) {
+  return items.reduce((acc, item) => {
+    if (item instanceof Array) acc.push(...flatten(item))
+    else if (item) acc.push(item)
+
+    return acc
+  }, [])
+}
